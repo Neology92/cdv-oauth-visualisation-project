759ce2b22c0e4b548eecf1dd80734895009234f9
Neology92 <oskarlegner@gmail.com>
2020-06-23 21:27:26 +0200

    Add saving user to db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys.js');
+const User = require('../models/user-model');
+const mongoose = require('mongoose');
 
 passport.use(
 	new GoogleStrategy(
@@ -11,6 +13,15 @@ passport.use(
 			clientSecret: keys.google.client_secret,
 		},
 		(accessToken, refreshTOken, profile, done) => {
+			new User({
+				username: profile.displayName,
+				googleID: profile.id,
+			})
+				.save()
+				.then((newUser) => {
+					console.log('Created new user: ' + newUser);
+				});
+
 			done();
 		}
 	)
--- models/user-model.js
+++ models/user-model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 	username: String,
-	googleId: String,
+	googleID: String,
 });
 
 const User = mongoose.model('user', userSchema);
