66f1b42e54bf9677a9d4868e4c4e6c7732305303
Neology92 <oskarlegner@gmail.com>
2020-06-23 16:52:34 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+// setup view engine
+app.set('view engine', 'ejs');
+
+// create home route
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+app.listen(3000, () => {
+	console.log('App listening on port 3000');
+});
