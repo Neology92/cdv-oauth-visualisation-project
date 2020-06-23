5f5b409bc9a575e7748a4d0ad049aae2e1b00375
Neology92 <oskarlegner@gmail.com>
2020-06-23 21:14:06 +0200

    Connect to mongodb

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ let path = require('path');
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
 // setup view engine
 app.set('view engine', 'ejs');
+
+// connect to mongodb
+mongoose.connect(
+	keys.mongodb.dbURI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => {
+		console.log('Connected to mongodb');
+	}
+);
 
 // set up routes
 app.use('/auth', authRoutes);
